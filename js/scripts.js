83a2b53097d0b3169c3e4586b3d10ebb0b21b130
Daniel Commins <daniel.commins@techelevator.com>
2023-09-01 13:43:42 -0700

    initial draft

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+const studentFanPageUrls = new Map();
+
+studentFanPageUrls.set('Brett Williams', "https://brwilliams1.github.io/fan-page/");
+studentFanPageUrls.set('John Joyce', "https://webcr8tor.github.io/fan-page/");
+studentFanPageUrls.set('Emily Sanders', "https://emilyesanders.github.io/fan-page/");
+studentFanPageUrls.set('Dj Litts', "https://littsdj.github.io/fan-page/");
+studentFanPageUrls.set('Jose Reveles', "https://jrev66.github.io/fan-page/");
+studentFanPageUrls.set('Connor Finnegan', "https://finneg23.github.io/fan-page/");
+studentFanPageUrls.set('Maryam Mian', "https://mianms.github.io/fan-page/");
+studentFanPageUrls.set('Jay Smith', "https://jaysmith413.github.io/fan-page/");
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    const mainEl = document.querySelector('main.fan-page-cards');
+    const template = document.getElementById('card-template');
+
+    for(const [name, url] of studentFanPageUrls){
+        const clone = document.importNode(template.content, true);
+        clone.querySelector('.card-header').innerText = name;
+        clone.querySelector('iframe').src = url;
+        clone.querySelector('.fan-site-button').addEventListener('click', openSiteInTab);
+        mainEl.appendChild(clone);
+    }
+});
+
+function openSiteInTab(event){
+    const iframeSite = event.target.previousElementSibling;
+    const url = iframeSite.getAttribute('src');
+    const w = window.open(url, "_blank");
+}
